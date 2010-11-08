eba680ece05c8533adf1da11fbd032080de59e3f
George Notaras <gnot@g-loaded.eu>
2010-11-08 08:54:17 +0200

    Improved features

--- src/PostgreSQL_manager/__init__.py
+++ src/PostgreSQL_manager/__init__.py
@@ -45,8 +45,8 @@ django-postgresql-manager currently supports:
 - Database ownership changing.
 - Control over the connection limit for users and databases.
 - Marking a user as not active.
-- Multiple PostgreSQL roles per Django user. Each user has control over the
-  roles it has created.
+- Multiple PostgreSQL roles and databases per Django user. Each user has
+  control over the roles and databases it has created.
 
 The distribution package includes an example project, which can be used to
 demonstrate the application quickly.
